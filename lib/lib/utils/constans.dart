2f007ba42c33d774e33b547ae0580799cf57bf86
Ericka Gonzalez Romero <erickagr119@gmail.com>
2024-09-18 00:30:23 -0600

    Diseño de la vista login

---
+++ lib/lib/utils/constans.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const List listaExamen=[
+  //‘id # número # titulo # descripción # num de estrellas en amarillo’,
+  //‘1 # 10 # Comprar pastel # Pasar a la pastelería para comprar un pastel de chocolate # 2’,
+  '1#5 #Comprar pastel #Pasar a la pastelería para comprar un pastel de chocolate #1',
+  '2#6 #Tarea de Criptografia #Subir la tarea 2 a didactic #2',
+  '3#7 #Examen de Movil 32#Realizar el examen de 1er parcial y subirlo a github #5',
+  '4#8 #Despensa #Ir a comprar la despensa semanal #2',
+  '5#9 #Tarea de Administracion #Realizar mapa mental de todo el parcial #3',
+  '6#10 #Servicio social #Realizar el avance de el sistema #2',
+  '7#11 #Apuntes de criptografia #Completar apuntes del parcial 2 #2',
+  '8#12 #Limpieza #Limpieza de la casa, cocina baños y cuarto #2',
+  '9#13 #Examen de Administracion #Estudiar para el examen de administracion #1',
+  '10#14 #Lavar ropa #Meter a lavar la ropa blanca #2',
+  '11#15 #Salida con amigas #Acordar la hora y lugar de la salida #2',
+  '12#16 #Leer libro #Comenzar el nuevo libro #1',
+
+];
+
+const fondo = Color(0xff520120);
+const fondo2 = Color(0xff64011f);
+const fondo3 = Color(0xff41011a);
+const fondo4 = Color(0xffb20222);
+const titulos = Color(0xff948800);
+const botones = Color(0xffa65476);
+const efectos = Color(0xff919b02);
+
---
+++ lib/login.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:examen1egr/lib/utils/constans.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final usuario=TextEditingController();
+  TextEditingController pass= TextEditingController();
+  final List<Color> colors = [fondo, fondo2, fondo3, fondo4];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: fondo,
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              recuadro(numFlex: 4, color1: fondo2, color2: fondo, color3: fondo3, color4: fondo4),
+              recuadro(numFlex: 4, color1: fondo3, color2: fondo4, color3: fondo2, color4: fondo),
+              recuadro(numFlex: 4, color1: fondo4, color2: fondo3,color3: fondo, color4: fondo2),
+            ],
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(30),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.7),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      'Bienvenido a tu primer EXAMEN',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: titulos,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: usuario,
+                      decoration: InputDecoration(
+                        fillColor: Colors.white, filled: true,
+                        hintText: 'Correo/Usuario',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: const BorderSide(width: 2),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: pass,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        fillColor: Colors.white, filled: true,
+                        hintText: 'Contraseña',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: const BorderSide(width: 2),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: botones,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        fixedSize: const Size(200, 40),
+                      ),
+                      onPressed: () {
+
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          SizedBox(width: 10),
+                          const Text('Iniciar Sesión',
+                            style: TextStyle(fontSize: 16, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    const Text('Mi primer examen, ¿estará sencillo?',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+class recuadro extends StatelessWidget {
+  final int numFlex;
+  final Color color1;
+  final Color color2;
+  final Color color3;
+  final Color color4;
+
+  const recuadro({
+    super.key,
+    required this.numFlex,
+    required this.color1,
+    required this.color2,
+    required this.color3,
+    required this.color4,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: numFlex,
+      child: Row(
+        children: [
+          expandedFuncion(colores: color1),
+          expandedFuncion(colores: color2),
+          expandedFuncion(colores: color3),
+          expandedFuncion(colores: color4),
+        ],
+      ),
+    );
+  }
+}
+
+class expandedFuncion extends StatelessWidget {
+  final Color colores;
+
+  const expandedFuncion({
+    super.key,
+    required this.colores,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: colores,
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    );
+  }
+}
+
